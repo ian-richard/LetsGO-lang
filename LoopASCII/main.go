f072af21e8d14d0d6a94005c991e5ee5ba9851b9
Ian F <ian.fiddeman@gmail.com>
2021-11-29 14:14:32 +0000

    tidying up loops

--- LoopASCII/main.go
+++ LoopASCII/main.go
@@ -3,8 +3,20 @@ package main
 import ("fmt")
 
 func main() {
-	for i := 33; i <= 122; i++ {
-		fmt.Printf("%v\t%#x\t%#U\n", i, i, i)
-		//num, hexidecimal, unicode + character 
+	//printnums(10)
+	ascii()
+}
+
+//func which prints every number between 1 and 10
+func printnums(num int) {
+	for i := 1; i <= num; i++ {
+		fmt.Println(i)
 	}
 }
+// prints every character in as num, hexidecimal, unicode + character 
+func ascii(){
+	for i := 33; i <= 122; i++ {
+			fmt.Printf("%v\t%#x\t%#U\n", i, i, i)
+			//num, hexidecimal, unicode + character 
+		}
+	}
