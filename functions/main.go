39325180a08bfe2d626c0390a2c320f93145d621
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-02 15:28:43 +0000

    method added to value type Person (struct)

--- functions/main.go
+++ functions/main.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println(sumSlice([]int{1,2,3,4}...))
-	fmt.Println(sumSlice(1,2,3))
+	// fmt.Println(sumSlice([]int{1,2,3,4}...))
+	// fmt.Println(sumSlice(1,2,3))
 
-	fmt.Println(sumSlice__notVariadic([]int{1,2,3,4}))
+	// fmt.Println(sumSlice__notVariadic([]int{1,2,3,4}))
+	fmt.Println(p.speak())
 	}
 
 func sumSlice(numbs ...int) int {
@@ -25,12 +26,33 @@ func sumSlice__notVariadic(numbs []int) int {
 	return result
 }
 
-func foo(num int) int {
-	return num
-	}
+type Person struct {
+	firstname string
+	lastname string
+	age int
+}
 
-func bar (num int, s string) (int, string) {
-	return num, s
-	}
+var p = Person{
+	firstname: "James",
+	lastname: "Bond",
+	age: 45,
+}
+
+func (p Person) speak() string {
+	return fmt.Sprintf("I'm %s %s and I'm %d years old", p.firstname, p.lastname, p.age)
+}
+
+func (p Person) getNameAndAge() string {
+	return fmt.Sprintf("%s %s is %d years old", p.firstname, p.lastname, p.age)
+}
+
+
+// func foo(num int) int {
+// 	return num
+// 	}
+
+// func bar (num int, s string) (int, string) {
+// 	return num, s
+// 	}
 
 
