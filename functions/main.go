f2787437f426ccf24853f5e3c9e1601a41c2fb42
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-02 14:39:16 +0000

    function ex.1 added

--- functions/main.go
+++ functions/main.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println(foo(42))
-	fmt.Println(bar(42, "hello"))
+	fmt.Println(sumSlice([]int{1,2,3,4}...))
 	}
+
+func sumSlice(numbs ...int) int {
+	result := 0
+	for _, num := range numbs {
+		result += num
+	}
+	return result
+}
 
 func foo(num int) int {
 	return num
