82198437733fab0fa7b0edcb44321c62922422b1
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-03 13:39:11 +0000

    callback exercise added

--- callback/main.go
+++ callback/main.go
@@ -1,6 +1,28 @@
 package main
 
+import "fmt"
+
 func main () {
+	g := func(xi []int) int {
+		if len(xi) == 0 {
+			return 0
+		}
+		if len(xi) == 1 {
+			return xi[0]
+		}
+		return xi[0] + xi[len(xi) - 1]
+	}
+
+	callbackInAction := foo(g, []int{1, 2, 3, 4, 5})
+	fmt.Println(callbackInAction)
 	
 }
+
+func foo(f func(xi []int) int, ii []int) int {
+	n := f(ii)
+	n--
+	return n
+}
+
+
 
