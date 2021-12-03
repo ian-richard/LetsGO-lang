772030d8fa9b97049f2833851c6b30651b411882
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-03 13:42:09 +0000

    notes on callback added

--- callback/main.go
+++ callback/main.go
@@ -24,5 +24,10 @@ func foo(f func(xi []int) int, ii []int) int {
 	return n
 }
 
+//a real life example might be a salary calculation func, which is then passed another function to add or reduce pay based on number of holiday days taken/or not
+//ie.
+//the function that is passed to the salary calculation func is a callback function
+//the callback function is then called by the salary calculation func
+
 
 
