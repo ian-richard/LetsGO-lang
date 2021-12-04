6c5efb5c3c42ab4f1d5ea936ce75fa4dc2fe6803
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-04 15:39:02 +0000

    using pointers to change values

--- pointer/main.go
+++ pointer/main.go
@@ -2,9 +2,33 @@ package main
 
 import "fmt"
 
-func main (){
-	v := 1
-	fmt.Println(&v)
+//create a person struct and func called ChangeMe as parameter
+//to dereference a struct, use the (*value).field syntax
+//reassign the value of the struct to the pointer. i.e "bob" to "alice"
+//print out the pointer and the value of the struct
 
+type person struct {
+	name string
+}
+
+func main (){
+	//ex. address in memory of the variable
+	// v := 1
+	// fmt.Println(&v)
+	
+	p1 := person {
+		name: "Bob",
+	}
+
+	fmt.Println(&p1)
+	fmt.Println(p1)
+	changeName(&p1)
+	fmt.Println(&p1)
+
+}
+
+//function which takes in a *person as a parameter
+func changeName(p *person) {
+	p.name = "Alice"
 }
 
