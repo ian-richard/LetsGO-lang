546ed4c2599638758ad5d58407ee7c07fe145aea
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-17 14:09:00 +0000

    custom sort exercise added

--- MarshalExampleToJson/main.go
+++ MarshalExampleToJson/main.go
@@ -10,6 +10,11 @@ type user struct {
 	Age   int
 }
 
+type jsonToGoUser struct {
+	First string `json:"First"`
+	Age   int    `json:"Age"`
+}
+
 func main() {
 	u1 := user{
 		First: "James",
@@ -22,13 +27,30 @@ func main() {
 	}
 
 	users := []user{u1, u2}
+	fmt.Println("users in GO: ", users)
 
-	fmt.Println(users)
-
+	//Marshal data into JSON
 	output, err := json.Marshal(users)
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(string(output))
+	fmt.Println("JSON: ", string(output))
+
+	//Unmarshal JSON into data
+	var jsonToGoUsers []jsonToGoUser
+	err = json.Unmarshal(output, &jsonToGoUsers)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("JSON to GO: ", jsonToGoUsers)
+
+	//loop over
+	fmt.Println("\nLoop over JSON to GO: ")
+	for i, v := range jsonToGoUsers {
+		fmt.Println("\nPerson number:", i)
+		fmt.Println("First Name:", v.First)
+		fmt.Println("Age:", v.Age)
+	}
 
 }
