288011572c749a488a325f566fb488530351fbbf
Ian-Richard <ian.fiddeman@gmail.com>
2021-12-20 12:01:24 +0000

    race condition added

---
+++ RaceCondition/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+//NB: Race conditions are problematic. below is an example of bad code. see here for more info and diagram https://www.ardanlabs.com/blog/2013/09/detecting-race-conditions-with-go.html
+
+func main() {
+	fmt.Println("CPUs:", runtime.NumCPU())
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+
+	counter := 0
+
+	const gs = 100
+	var wg sync.WaitGroup
+	wg.Add(gs)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			v := counter
+			// time.Sleep(time.Second)
+			runtime.Gosched()
+			v++
+			counter = v
+			wg.Done()
+		}()
+		fmt.Println("Goroutines:", runtime.NumGoroutine())
+	}
+	wg.Wait()
+	fmt.Println("Goroutines:", runtime.NumGoroutine())
+	fmt.Println("count:", counter)
+}
