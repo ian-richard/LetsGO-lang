e26559dc15ea75ec496e6aad7dedbeb8920812c3
Ian <68618201+ian-richard@users.noreply.github.com>
2022-03-26 10:18:26 +0000

    refactor exercise

--- CodeWars/SliceOfGamePoints/main.go
+++ CodeWars/SliceOfGamePoints/main.go
@@ -6,7 +6,31 @@ import ( "fmt"
 func main(){
 	result := Points([]string{"1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"})
 	fmt.Println(result == 15)
+
+	result1 := PointsRefactor([]string{"1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"})
+	fmt.Println(result1 == 15)
+
+	result2 := PointsWithRange([]string{"1:0","2:0","3:0","4:0","2:1","1:3","1:4","2:3","2:4","3:4"})
+	fmt.Println(result2 == 15)
+
+
 }
+
+
+func PointsWithRange(games []string) int {
+	result := 0
+	for _, game := range games {
+	  str := s.Split(game, ":")
+	  x, y := str[0], str[1]
+	  switch {
+		case x > y:
+		  result += 3
+		case x == y:
+		  result += 1
+	  }
+	}
+	return result
+  }
 
 func Points(games []string) int {
   result := 0
@@ -24,3 +48,15 @@ func Points(games []string) int {
   }
   return result;
 }
+
+func PointsRefactor(games []string) int {
+	points := 0
+	for _, g := range games {
+		if g[0]>g[2] {
+		  points += 3
+		} else if g[0] == g[2] {
+		  points += 1
+		}
+	}
+	return points
+  }
